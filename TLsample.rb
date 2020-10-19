f8530b2c2feb0e183df8891fb9afff4e05138f5b
Malcolm Gin <malcolm.gin@gmail.com>
2020-10-19 11:31:06 -0700

    Update TLsample.rb
    Updated with more pretty output.

--- TLsample.rb
+++ TLsample.rb
@@ -1,4 +1,4 @@
-# Ruby (w/o) Rails, developed by Malcolm Gin on macOS Catalina with ruby 2.6.3p62 (2019-04-16 revision 67580) [universal.x86_64-darwin19] 
+# Ruby (w/o) Rails, developed by Malcolm Gin on macOS Catalina with ruby 2.6.3p62 (2019-04-16 revision 67580) [universal.x86_64-darwin19]
 # Based on sample code from https://www.twilio.com/blog/2015/10/4-ways-to-parse-a-json-api-with-ruby.html
 require 'net/http'
 require 'json'
@@ -12,4 +12,4 @@ parsed_json = JSON.parse(response)
 ans_value = parsed_json["answered"]
 
 # Based on 3rd idiom at https://www.programming-idioms.org/idiom/173/format-a-number-with-grouped-thousands/2440/ruby
-puts ans_value.to_s.gsub(/\B(?=(...)*\b)/, ',')
+puts "Answered value, with thousands comma separators: " + ans_value.to_s.gsub(/\B(?=(...)*\b)/, ',')
